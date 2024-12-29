9c0841c534390c73d236bb3dee1bc4f89e0edbec
JOEY <no_email@gmail.com>
2024-12-29 16:33:43 +0800

    推荐完成

--- app/src/leanback/java/com/fongmi/android/tv/ui/presenter/HeaderPresenter.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/presenter/HeaderPresenter.java
@@ -1,13 +1,17 @@
 package com.fongmi.android.tv.ui.presenter;
 
+import static android.os.Build.VERSION_CODES.R;
+
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.leanback.widget.Presenter;
 
+import com.fongmi.android.tv.R;
 import com.fongmi.android.tv.databinding.AdapterHeaderBinding;
 import com.fongmi.android.tv.utils.ResUtil;
+import com.fongmi.quickjs.bean.Res;
 
 public class HeaderPresenter extends Presenter {
 
@@ -19,7 +23,12 @@ public class HeaderPresenter extends Presenter {
     @Override
     public void onBindViewHolder(Presenter.ViewHolder viewHolder, Object object) {
         HeaderPresenter.ViewHolder holder = (HeaderPresenter.ViewHolder) viewHolder;
-        holder.binding.text.setText(object instanceof String ? object.toString() : ResUtil.getString((int) object));
+        String text = object instanceof String ? object.toString() : ResUtil.getString((int) object);
+        holder.binding.text.setText(text);
+        if (text == ResUtil.getString(com.fongmi.android.tv.R.string.home_recommend)){
+            // 设置marigin top
+            holder.binding.text.setPadding(0, ResUtil.dp2px(10), 0, 0);
+        }
     }
 
     @Override
